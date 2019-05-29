c21fd2c0f4ddaf8ae9bf642201ae40b90411488c
xuailin <xal18123303054@163.com>
2019-05-29 17:48:43 +0800

    网页高可用监控脚本

---
+++ jiancha.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+a=0cc0b5fc62af6f35638b637ecb990fb8
+while :
+  do
+    for j in 12 13
+     do
+       m=`curl 192.168.4.${j} 2> /dev/null | md5sum`
+       if [ ${m:0:32} != $a ];then
+          ipvsadm -ln | grep ${j} &> /dev/null
+         [ $? -eq 0 ] && ipvsadm -d -t 192.168.4.20:80 -r 192.168.4.${j}
+      else
+         ipvsadm -ln | grep ${j} &> /dev/null
+         [ $? -ne 0 ] && systemctl restart keepalived && iptables -F
+       fi
+     done
+   done
